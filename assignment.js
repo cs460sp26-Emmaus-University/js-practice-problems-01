e320372713db38d32db451bcf94be7bd20e7555d
Arthur Manning <124621648+renaissancenator@users.noreply.github.com>
2026-03-19 10:20:03 -0500

    Add initial function stubs for assignment tasks

---
+++ assignment.js
@@ -0,0 +1,40 @@
+// --- LOGIC & LOOPS ---
+
+// 1. Return the sum of all numbers in an array
+function sumArray(arr) { /* TODO */ }
+
+// 2. Return only numbers greater than 10
+function filterHighNumbers(arr) { /* TODO */ }
+
+// 3. FizzBuzz: Return "Fizz" if num is divisible by 3, "Buzz" if by 5, and "FizzBuzz" if both. Otherwise return the num.
+function fizzBuzz(num) { /* TODO */ }
+
+// 4. Find the largest number in an array
+function findMax(arr) { /* TODO */ }
+
+// --- ARRAYS & OBJECTS ---
+
+// 5. Given an array of strings, return an array of their lengths
+function getStringLengths(arr) { /* TODO */ }
+
+// 6. Create an object representing a "User" with name and age properties
+function createUser(name, age) { /* TODO */ }
+
+// 7. Given an array of User objects, return only the names of users over 18
+function getAdultNames(users) { /* TODO */ }
+
+// 8. Transform an array of strings to "Title Case" (first letter capitalized)
+function titleCase(arr) { /* TODO */ }
+
+// --- ADVANCED ---
+
+// 9. Count how many times a specific character appears in a string
+function countChar(str, char) { /* TODO */ }
+
+// 10. Check if an array is a palindrome (reads the same forwards and backwards)
+function isPalindrome(arr) { /* TODO */ }
+
+// DO NOT DELETE: Required for auto-grading
+if (typeof module !== 'undefined') {
+    module.exports = { sumArray, filterHighNumbers, fizzBuzz, findMax, getStringLengths, createUser, getAdultNames, titleCase, countChar, isPalindrome };
+}
